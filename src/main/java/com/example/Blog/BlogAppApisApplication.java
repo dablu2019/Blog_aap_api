bede0ae21bd1581de3cb9be95c83490887f8ed6b
dablu2019 <kdablu2019@gmail.com>
2022-10-15 20:58:09 +0530

    Complete all apis

--- src/main/java/com/example/Blog/BlogAppApisApplication.java
+++ src/main/java/com/example/Blog/BlogAppApisApplication.java
@@ -1,13 +1,28 @@
 package com.example.Blog;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.Blog.config.AppConstants;
+import com.example.Blog.entities.Role;
+import com.example.Blog.repositories.RoleRepo;
 
 @SpringBootApplication
-public class BlogAppApisApplication {
+public class BlogAppApisApplication implements CommandLineRunner {
 
+	
+	 @Autowired
+	private PasswordEncoder passwordEncoder;
+	 
+	 @Autowired
+	 private RoleRepo roleRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
 	}
@@ -18,4 +33,31 @@ public class BlogAppApisApplication {
 		
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+	System.out.println(this.passwordEncoder.encode("xyz"));	
+	
+	try {
+		Role role=new Role();
+		role.setId(AppConstants.ADMIN_USERS);
+		role.setName("ADMIN_USER");
+		
+		Role role1=new Role();
+		role1.setId(AppConstants.NORMAL_USERS);
+		role1.setName("NORMAL_USER");
+		
+		List<Role> roles=List.of(role,role1);
+		List <Role> result=this.roleRepo.saveAll(roles);
+		result.forEach(r->{
+			System.out.println(r.getName());
+		});
+		
+	}
+	catch(Exception e){
+		e.printStackTrace();
+		
+	}
+
+}
 }
